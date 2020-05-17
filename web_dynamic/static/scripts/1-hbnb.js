8e334b9ada2831777f22ed28653ad7a6ecfe0a4f
yasmineholb <1055@holbertonschool.com>
2020-05-17 15:01:32 +0200

    fix

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,13 @@
-$(document).ready(function (){
-  let doc = {};
-  $('INPUT:checkbox').change(function() {
+const $ = window.$;
+$(document).ready(function () {
+  const doc = {};
+  $('INPUT:checkbox').change(function () {
     if (this.checked) {
       doc[$(this).data('id')] = $(this).data('name');
-    }
-    else {
+    } else {
       delete doc[$(this).data('id')];
     }
-    let val = object.values(doc);
+    const val = Object.values(doc);
     if (val.length > 0) {
       $('div.amenities > h4').text(val.join(', '));
     }
